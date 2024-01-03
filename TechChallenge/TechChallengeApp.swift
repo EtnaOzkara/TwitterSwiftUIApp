3c8d26438c355a1761529f90125d07d767b2ccec
Etna Ozkara <etnaozkara@gmail.com>
2024-01-04 00:28:32 +0300

    Initial Commit

---
+++ TechChallenge/TechChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechChallengeApp.swift
+//  TechChallenge
+//
+//  Created by Etna Ozkara on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TechChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
